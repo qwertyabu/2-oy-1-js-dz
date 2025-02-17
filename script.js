c399556188ba64db6f8d63cdb7a05bbcdd72f4d1
qwertyabu <monkey.famil4@gmail.com>
2025-02-17 20:17:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector(".but1").addEventListener("click", function () {
+    document.body.style.backgroundColor = "#C4B5A5";
+    document.body.style.color = "black";
+});
+
+document.querySelector(".but2").addEventListener("click", function () {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+});
+
